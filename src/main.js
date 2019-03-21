d3c1e06587eea62abb342c3a824ee54ddb5cc4c4
Gabriel Leung <leun0077@algonquinlive.com>
2019-03-21 13:20:11 -0400

    Added-google-api

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import VueRouter from 'vue-router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import VueForm from 'vue-form';
+import * as VueGoogleMaps form 'vue2-google-maps'
 
 // internal libs
 import router from './router';
@@ -15,6 +16,7 @@ Vue.config.productionTip = false;
 Vue.use(VueRouter);
 Vue.use(ElementUI);
 Vue.use(VueForm);
+Vue.use{key: 'AIzaSyArIba7jYFIwBPBDgXD0r6vsWoXnQCD3QA', libraries: 'places'}
 
 new Vue({
   router,
