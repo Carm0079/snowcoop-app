4bfb5942936db2bb2bdb0bdb9f72eb280ef8a86e
Alex Carmichael <carm0079@algonquinlive.com>
2019-04-11 12:06:08 -0400

    fixed little things in assg2

--- src/components/pages/list-page/list-page.component.vue
+++ src/components/pages/list-page/list-page.component.vue
@@ -2,7 +2,7 @@
   <div id="listPage">
     <div class="list-wrapper">
       <div class="list-header">
-        <span class="list-title">{{`You have ${addressList.length}addresses to complete`}}</span>
+        <span class="list-title">{{`You have ${addressList.length} addresses left to complete`}}</span>
       </div>
       <div class="list-view-wrapper">
         <el-card class="map-view-card">
