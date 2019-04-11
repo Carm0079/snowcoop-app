a3eed2dd7af22d4a070bba1d345e021d770799b0
Gabriel Leung <leun0077@algonquinlive.com>
2019-04-11 11:39:19 -0400

    Assg2-complete

--- src/components/pages/list-page/list-page.component.vue
+++ src/components/pages/list-page/list-page.component.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="listPage">
-    <div class="listwrapper">
-      <div class="map-view-wrapper">
-        <map-view v-if="addressList" v-bind:showList="true" v-bind:addressList="addressList"></map-view>
+    <div class="list-wrapper">
+      <div class="list-header">
+        <span class="list-title">{{`You have ${addressList.length}addresses to complete`}}</span>
+      </div>
+      <div class="list-view-wrapper">
+        <el-card class="map-view-card">
+          <map-view v-if="addressList" v-bind:showList="true" v-bind:addressList="addressList"></map-view>
+        </el-card>
       </div>
     </div>
-    <p class="title">Address Page</p>
   </div>
 </template>
 <script src="./list-page.component.js"></script>
